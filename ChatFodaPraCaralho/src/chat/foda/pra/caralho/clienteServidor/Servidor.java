467b785711e9d98b162e9498cc1fec0f076cdd8c
Dr. Octopus <alessandro.belini_science@hotmail.com>
2014-05-23 19:27:02 -0300

    Update Servidor.java

--- ChatFodaPraCaralho/src/chat/foda/pra/caralho/clienteServidor/Servidor.java
+++ ChatFodaPraCaralho/src/chat/foda/pra/caralho/clienteServidor/Servidor.java
@@ -2,11 +2,7 @@ package chat.foda.pra.caralho.clienteServidor;
 
 public class Servidor {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
