aee1c6af85e44dc636f56dde6899c67c5166d81f
Dr. Octopus <alessandro.belini_science@hotmail.com>
2014-06-11 19:17:29 -0300

    Update GerenciadorDoBanco.java

--- ChatFodaPraCaralho/src/chat/foda/pra/caralho/bancoDados/GerenciadorDoBanco.java
+++ ChatFodaPraCaralho/src/chat/foda/pra/caralho/bancoDados/GerenciadorDoBanco.java
@@ -1,45 +1,55 @@
 package chat.foda.pra.caralho.bancoDados;
 
-import chat.foda.pra.caralho.modelo.Chat;
+import java.util.ArrayList;
+
+import chat.foda.pra.caralho.modelo.Usuario;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
 
 public class GerenciadorDoBanco {
-	
-	private final ObjectContainer bancoDeDados = Db4oEmbedded.openFile(
-			Db4oEmbedded.newConfiguration(), "bancoDeDados.db4o");
 
-	public void salva(Object object) {
-		bancoDeDados.store(object);
+	private ObjectContainer db;
+
+	// nomeBanco sem extensão
+	public void abrir(String nomeBanco) {
+		db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), nomeBanco
+				+ ".db4o");
 	}
 
-	public ObjectSet<Chat> listarChats() {
-		return bancoDeDados.query(Chat.class);
+	public ObjectSet<Usuario> getListaUsuarios() {
+		return db.query(Usuario.class);
+	}
+
+	public Usuario getUsuario(Integer codigo) {
+		ObjectSet<Usuario> lista = getListaUsuarios();
+		for (Usuario o : lista) {
+			if (o.getCodigo().equals(codigo)) {
+				return o;
+			}
+		}
+
+		return null;
+	}
+
+	public Usuario getUsuario(String apelido) {
+		ObjectSet<Usuario> lista = getListaUsuarios();
+		for (Usuario o : lista) {
+			if (o.getNickName().equals(apelido)) {
+				return o;
+			}
+		}
+
+		return null;
+	}
+
+	public void armazenar(Object objeto) {
+		db.store(objeto);
 	}
 
 	public void sair() {
-		bancoDeDados.close();
+		db.close();
 	}
 
-	public Chat getChatPorCodigo(Integer codigo) {
-		Chat chat = new Chat();
-		chat.setCodigo(codigo);
-		return (Chat) bancoDeDados.queryByExample(chat).get(0);
-	}
-
-	public static void main(String[] args) {
-		GerenciadorDoBanco main = new GerenciadorDoBanco();
-		try {
-			for(Chat c : main.listarChats()) {
-				System.out.println(c.getCodigo());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			main.sair();
-		} finally {
-			main.sair();
-		}
-	}
 }
